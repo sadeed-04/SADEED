be4be90378ef46f51a7d891c064c7beaf39020de
Mohammad SadeedHasan <sadeedhasan7@gmail.com>
2025-08-13 11:47:00 +0530

    CALCULAS

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# Basic Calculator
+a = 10 
+b = 20
+print("addition of two number is : " , a + b)
+print("subtraction of two number is : " , a - b)
+print("multiplication of two number is : " , a * b)
+print("division of two number is : " , a / b)
